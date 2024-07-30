10b1b218ce101185b12a89dccd45f389f838332b
5qU4llV777 <claudio.nogueira888@hotmail.com>
2024-07-30 07:57:20 -0400

    implementação da camada de dominio

---
+++ src/main/java/nogueira/springrailway/model/Feature.java
@@ -0,0 +1,8 @@
+package nogueira.springrailway.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/nogueira/springrailway/model/News.java
@@ -0,0 +1,8 @@
+package nogueira.springrailway.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
