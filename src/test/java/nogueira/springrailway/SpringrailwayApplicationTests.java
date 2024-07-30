218479f8ce6703f898f1b1bb182c7920d7eaf9c2
dragonborn <claudio.nogueira888@hotmail.com>
2024-07-30 06:07:07 -0400

    Initial commit

---
+++ src/test/java/nogueira/springrailway/SpringrailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package nogueira.springrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
